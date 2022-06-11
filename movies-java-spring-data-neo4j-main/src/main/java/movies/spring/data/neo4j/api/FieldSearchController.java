fd8aaa0d262215d4eaf8283367731a2c2f87b540
LWF0824 <985579124@qq.com>
2022-06-11 20:27:41 +0800

    搜索

---
+++ movies-java-spring-data-neo4j-main/src/main/java/movies/spring/data/neo4j/api/FieldSearchController.java
@@ -0,0 +1,9 @@
+package movies.spring.data.neo4j.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class FieldSearchController {
+}
